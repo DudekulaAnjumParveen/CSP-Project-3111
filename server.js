a2b0347ae9461173e0c786f615a32b2ea211bfa9
DudekulaAnjumParveen <dudekulaanjumparveen@gmail.com>
2026-05-31 19:14:02 +0530

    Updated port for Render deployment

--- server.js
+++ server.js
@@ -156,10 +156,12 @@ app.get('/solve/:id', async (req,res) => {
 });
 /* SERVER */
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
 
     console.log(
-        'Server running on port 3000'
+        `Server running on port ${PORT}`
     );
 
 });
